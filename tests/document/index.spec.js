4e4e940f3c2ddff1f79ee40b21621255062b7439
Shailen Naidoo <naidooshailen648@gmail.com>
2020-04-05 12:42:51 +0200

    Wrote some tests

--- tests/document/index.spec.js
+++ tests/document/index.spec.js
@@ -8,7 +8,32 @@ LocalVue.config.silent = true
 LocalVue.use(Vuevent)
 
 describe('Test document events', () => {
-  test('Dummy test', () => {
-    expect(1).toBe(1)
+  test('Check if click event fires and calls our handler', () => {
+    let vmRef = null
+
+    const documentListeners = {
+      click(e) {
+        vmRef = this
+      }
+    }
+  
+    const clickSpy = jest.spyOn(documentListeners, 'click')
+  
+    document.body.innerHTML = `<div id="app"></div>`
+
+    const vm = new LocalVue({
+      el: '#app',
+      render: (h) => (<span>Hello World</span>),
+      events: {
+        document: {
+          click: documentListeners.click
+        }
+      }
+    })
+
+    document.dispatchEvent(new MouseEvent('click'))
+
+    expect(clickSpy).toHaveBeenCalledTimes(1)
+    expect(vm._uid).toBe(vmRef._uid)
   })
 })
--- tests/window/index.spec.js
+++ tests/window/index.spec.js
@@ -8,7 +8,32 @@ LocalVue.config.silent = true
 LocalVue.use(Vuevent)
 
 describe('Test window events', () => {
-  test('Dummy test', () => {
-    expect(1).toBe(1)
+  test('Check if keypress event fires and keypress our handler', () => {
+    let vmRef = null
+
+    const windowListeners = {
+      keypress(e) {
+        vmRef = this
+      }
+    }
+  
+    const keypressSpy = jest.spyOn(windowListeners, 'keypress')
+  
+    document.body.innerHTML = `<div id="app"></div>`
+
+    const vm = new LocalVue({
+      el: '#app',
+      render: (h) => (<span>Hello World</span>),
+      events: {
+        window: {
+          keypress: windowListeners.keypress
+        }
+      }
+    })
+
+    window.dispatchEvent(new KeyboardEvent('keypress'))
+
+    expect(keypressSpy).toHaveBeenCalledTimes(1)
+    expect(vm._uid).toBe(vmRef._uid)
   })
 })
